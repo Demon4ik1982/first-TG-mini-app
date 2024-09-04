8ae22df3c82c578524c521a5a57d18a78c76c3b2
Dmitrii Shautin <shautind@yandex.ru>
2024-09-04 15:42:34 +0300

    second commit

--- src/api/getWeather.ts
+++ src/api/getWeather.ts
@@ -1,7 +1,7 @@
 import { Weather } from "../models/weather";
 
 export const getWeather = async (): Promise<Weather> => {
-  const responce = await fetch(`http://api.openweathermap.org/data/2.5/weather?id=498817&lang=ru&appid=056b499455952900069f6a5d4e1aa450`);
+  const responce = await fetch(`https://api.openweathermap.org/data/2.5/weather?id=498817&lang=ru&appid=056b499455952900069f6a5d4e1aa450`);
   const weather = await responce.json();
 
   return weather;
